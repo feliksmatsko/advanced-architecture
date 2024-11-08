e99664a8083c1d7513f8df46eb58ef744f1ae02d
Feliks Matsko <feliksir@gmail.com>
2024-11-08 14:37:12 +0200

    refactor to cqrs

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -1,11 +1,18 @@
 import { DynamicModule, Module, Type } from '@nestjs/common';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
 import { AlarmsController } from '../presenters/http/alarms.controller';
-import { AlarmsService } from './alarms.service';
+import { AlarmsFacade } from './alarms.facade';
+import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 
 @Module({
   controllers: [AlarmsController],
-  providers: [AlarmsService, AlarmFactory],
+  providers: [
+    AlarmsFacade,
+    AlarmFactory,
+    CreateAlarmCommandHandler,
+    GetAlarmsQueryHandler,
+  ],
 })
 export class AlarmsModule {
   static withInfrastructure(infrastructureModule: Type | DynamicModule) {
